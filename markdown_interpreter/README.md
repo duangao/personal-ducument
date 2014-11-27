ad91fabea8118924506d37d38761929d41c96c30
duangao <gaoduan0306@gmail.com>
2014-11-28 00:38:24 +0800

    new file:   markdown_interpreter/README.md 	new file:   markdown_interpreter/example.html 	new file:   markdown_interpreter/example.md 	new file:   markdown_interpreter/markdown_interpreter.py

---
+++ markdown_interpreter/README.md
@@ -0,0 +1,27 @@
+# This is a simple interpreter which can get .md file and convert it to html.
+
++ This project is completed by python 3.4 and module os 、 re
++ I convert the markdownw file to simple html format , only use the simplest html syntax.
+
+# Now,it can support functions as follows:
++ list : both ordered or not.
++ 6 levels header
++ horizontal line
++ superscript，format:
+
+>   [text][sub]
+
++ quote
++ block quote
++ code block and highlight
+ 
+> you can add more syntex in source code,now I only support def and print() keywords.
+
++ link 
+
+# More
++ File example.md is one example file , and example.html is the output.
+
++ I use consolas font in html.
+
+> If you find any bug when you use it , I would appreciate you if you can tell it to me.
---
+++ markdown_interpreter/markdown_interpreter.py
@@ -0,0 +1,212 @@
+__author__ = 'duangao'
+
+symbol_order_list=False
+symbol_unorder_list=False
+symbol_block_list=False
+symbol_code_block=False
+
+language=['python','c++']
+import os
+import re
+
+
+def handle_token(i):
+
+    if len(i)>5 and i[0]==i[1]==i[-1]==i[-2]=='*':      # deal with bold char
+        i="<b>"+handle_token(i[2:-2])+"</b>"
+    if len(i)>3 and i[0]==i[-1] and i[0]=='*':                       # deal with italic char
+            i="<i>"+handle_token(i[1:-1])+"</i>"
+    if len(i)>5 and i[0]==i[1]==i[-1]==i[-2]=='~':      # deal with delete line
+            i="<S>"+handle_token(i[2:-2])+"</S>"
+    if len(i)>7 and i[0]=="\\":                                    # deal with link
+        pattern=re.compile(r'(\\\[)(.*)(\])(\()(.*)\)')
+        a=pattern.match(i)
+        if a:
+            text,url=a.group(2,5)
+            i="<a href=\"" +url+"\" target=\"_blank\">"+text+"</a>"
+            print(i)
+        else:
+            i=i
+    if len(i)>6 and i[0]=="[":
+        pattern=re.compile(r'(\[)(.*)(\])(\[)(\d)(\])')
+        a=pattern.match(i)
+        if a:
+            text,sub=a.group(2,5)
+            i=text+"<sup>"+sub+"</sup>"
+        else:
+            i=i
+
+    else :
+        i=i
+    return i
+
+def token(string):
+    result=string.split()
+    str_result=" "
+
+    if len(result)==0:
+        return ""
+
+    sym=handle_token(result.pop(0))
+
+    for i in result:
+        i=handle_token(i)
+        str_result+=str(i)+" "
+
+
+    result=list([sym,str_result])
+    return result
+
+def set_symbol(*args):
+    for i in args:
+        i=False
+
+def close_list(result):
+    global symbol_order_list
+
+    result=""
+    if symbol_order_list==True:
+        result="</ol>"
+    symbol_order_list=False
+    return result
+
+
+
+def parse(line):
+    result=token(line)
+    global  symbol_code_block
+    global  symbol_block_list
+
+
+    if len(result)==0:
+        result=close_list(result)
+        return result
+
+    (symbol,content)=result
+
+
+    subsymbol=symbol.replace('.',' . ').split()
+
+    num=0
+
+    if len(subsymbol)==2:
+        (num,dot)=subsymbol
+        global symbol_order_list
+        symbol_order_list=True
+
+
+    # header
+    if symbol=='#':
+        result=close_list(result)
+        result+="<h1><font face=\"Consolas\">"+content+"</font></h1>"
+    elif symbol=='##':
+        result=close_list(result)
+        result+="<h2>"+content+"</h2>"
+
+    elif symbol=='###':
+        result=close_list(result)
+        result+="<h3>"+content+"</h3>"
+    elif symbol=='####':
+        result=close_list(result)
+        result+="<h4>"+content+"</h4>"
+    elif symbol=='#####':
+        result=close_list(result)
+        result+="<h5>"+content+"</h5>"
+    elif symbol=='######':
+        result=close_list(result)
+        result+="<h6>"+content+"</h6>"
+
+    # unorderd list
+    elif symbol=="+" or symbol=="*" or symbol=='-' :
+        result=close_list(result)
+        result+="<ul><li>"+content+"</li></ul>"
+
+
+
+    # quote
+    elif symbol=='>':
+        result=close_list(result)
+        result+="<blockquote><p style=\"color:#8fbc8f\"><strong><i>"+ content+"</i></strong></p></blockquote></br>"
+
+    elif symbol=="```":
+        result=close_list(result)
+
+        if symbol_block_list==False and symbol_code_block==False:
+            symbol_block_list=True
+            result+="<blockquote>"+content+"</br>"
+        elif symbol_block_list ==True or symbol_code_block==True:
+            symbol_block_list=False
+            symbol_code_block=False
+            result+="</blockquote>"
+
+    elif symbol[0:3]=="```" and symbol[3:] in language:
+        result=close_list(result)
+        if symbol_code_block == False:
+            symbol_code_block=True
+            result+="<blockquote style=\"background-color:#ffebcd\"></br>"
+            print(result)
+
+    # ordered list
+    elif symbol_order_list==True:
+        if num=="1":
+            result="<ol><li>"+content+"</li>"
+        else:
+            result="<li>"+content+"</li>"
+            symbol_order_list=True
+
+
+    # direct content
+    else:
+        content=result[0]+result[1]
+        result=close_list(result)
+        # handle with horiral line
+        count=0
+        for i in content:
+            if i=='-':
+                count+=1
+        if count==len(content):
+            result+="<hr>"
+        #########################
+        elif symbol_code_block==True:
+            for item in content.split():
+                if item=="def":
+                    result+="<font color=\"red\"> "+item+"</font> "
+                elif item[0:6] == "print(" and item[-1]==")":
+                    result+="<font color=\"blue\"> "+ item +"</font> "
+                else:
+                    result+="<font color=\"black\">"+item+"</font>"
+                    print(result)
+            result+="</br>"
+        else:
+            result+="<p>"+content+"</p>"
+
+        #return result
+
+
+    return result
+
+
+
+
+def main(loc):
+    file=open(loc,"r")
+    result=open("result.html","w")
+    symbol_unorder_list=False
+    symbol_order_list=False
+    result.write("<style type=\"text/css\">div {display: block;font-family: Consolas}#wrapper { width: 100%;"
+                 " height:100%; margin: 0; "
+                 " padding: 0;}#left { float:left;  width: 10%;  height: 100%;  }#second {   float:left;   width: 80%;"
+                 "   height: 100%;   }#right {float:left;  width: 10%;  height: 100%; "
+                 "}</style><div id=\"wrapper\"> <div id=\"left\"></div><div id=\"second\">")
+    for eachline in file:
+        l=(str)(parse(eachline))
+        result.write(l)
+    result.write("</div><div id=\"right\"></div></div>")
+
+
+location=input()
+
+name,ext=os.path.splitext(location)
+if ext=='.md':
+    main(location)
+
